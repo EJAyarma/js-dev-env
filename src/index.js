a4a1148de904642bc5eee0ecefca89c6e98a49f6
ejayarma <johnayarmahe@gmail.com>
2020-08-27 22:30:54 +0000

    first commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import './index.css';
+
+import numeral from 'numeral';
+
+const courseValue = numeral(1000).format('$0,0.00');
+
+/* eslint-disable no-console */
+// debugger;
+console.log(`I would like to pay ${courseValue} for this awesome course`);
